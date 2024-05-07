b844c07bf9d63982aefea44b09bfc2e3e9ba7d3f
HarukaTea <885756957@qq.com>
2024-05-07 12:39:31 +0800

    Revert "before update Fusion - 5/7"
    This reverts commit d1c7fa0285b28b0988276a4e6db733a542a25703.

---
+++ src/modules/Packages/Fusion/Utility/doNothing.lua
@@ -0,0 +1,12 @@
+--!nocheck
+--!optimize 2
+--!native
+
+--[[
+	An empty function. Often used as a destructor to indicate no destruction.
+]]
+
+local function doNothing(...: any)
+end
+
+return doNothing
