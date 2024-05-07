a3481d49b2f7def33cfee98da521db4961508fe2
HarukaTea <885756957@qq.com>
2024-05-07 16:51:32 +0800

    update Cmdr and Fusion - 5/7

--- src/modules/Packages/Cmdr/init.lua
+++ src/modules/Packages/Cmdr/init.lua
@@ -1,31 +1,31 @@
---!nocheck
---!optimize 2
---!native
 
-local RS = game:GetService("RunService")
 
-local Util = require(script.Shared.Util)
+local RunService = game:GetService("RunService")
+local Util = require(script.Shared:WaitForChild("Util"))
 
-if not RS:IsServer() then
-	error("Cmdr server module is somehow running on a client!")
+if RunService:IsServer() == false then
+	error(
+		"[Cmdr] Client scripts cannot require the server library. Please require the client library from the client to use Cmdr in your own code."
+	)
 end
 
-local Cmdr do
+local Cmdr
+do
 	Cmdr = setmetatable({
-		ReplicatedRoot = nil;
-		RemoteFunction = nil;
-		RemoteEvent = nil;
-		Util = Util;
-		DefaultCommandsFolder = script.BuiltInCommands;
+		ReplicatedRoot = nil,
+		RemoteFunction = nil,
+		RemoteEvent = nil,
+		Util = Util,
+		DefaultCommandsFolder = script.BuiltInCommands,
 	}, {
-		__index = function (self, k)
+		__index = function(self, k)
 			local r = self.Registry[k]
 			if r and type(r) == "function" then
-				return function (_, ...)
+				return function(_, ...)
 					return r(self.Registry, ...)
 				end
 			end
-		end
+		end,
 	})
 
 	Cmdr.Registry = require(script.Shared.Registry)(Cmdr)
@@ -36,7 +36,7 @@ end
 
 -- Handle command invocations from the clients.
 Cmdr.RemoteFunction.OnServerInvoke = function(player, text, options)
-	if #text > 100_000 then
+	if #text > 10000 then
 		return "Input too long"
 	end
 
